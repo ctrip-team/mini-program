f2646a9f32503061cea5be6209a25d0d2b49035b
finabin <13803371+finabin@user.noreply.gitee.com>
2024-03-23 17:07:31 +0800

    搜索栏/底部导航栏/展示组件初步搭建

---
+++ src/components/MyTabBar/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { AtTabBar } from 'taro-ui'
+
+
+
+export default function MyTabBar() {
+  const [current, setCurrent] = useState(0)
+  function handleClick(value) {
+    setCurrent(value)
+  }
+
+  const tabList = [
+    {
+      title: '首页',
+      iconType: 'list',
+      pagePath: '/pages/index/index'
+    },
+  ]
+
+  return (
+    <>
+      <AtTabBar
+        fixed
+        tabList={tabList}
+        onClick={handleClick()}
+        current={setCurrent(current)}
+      />
+    </>
+  )
+}
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,19 +1,32 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
+import { AtSearchBar } from 'taro-ui'
+import React, { useState } from 'react'
 import './index.scss'
-import MyTabBar from '../../components/TabBar'
+import MyTabBar from '../../components/MyTabBar'
+
 
 export default function Index() {
-
   useLoad(() => {
     console.log('Page loaded.')
   })
 
+  const [queryValue, setQueryValue] = useState('')
+
   return (
     <>
+      <AtSearchBar
+        value={queryValue}
+        onChange={value => setQueryValue(value)}
+        onActionClick={() => {
+          // 处理搜索逻辑
+          console.log('执行搜索操作');
+        }}
+      />
       <View className='index'>
         <Text>Hello world!</Text>
       </View>
+      {/* <MyTabBar /> */}
     </>
   )
 }
