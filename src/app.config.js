e80702215b79d1ba0ba12eea6fc087eca38a62ea
finabin <13803371+finabin@user.noreply.gitee.com>
2024-03-24 15:37:09 +0800

    完善了首页基本样式框架，重新修改了tabbar

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,32 @@
 export default defineAppConfig({
   pages: [
     'pages/index/index',
+    'pages/my/my',
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    color: "#333",
+    selectedColor: "#ff0000",
+    backgroundColor: "#fff",
+    borderStyle: "black",
+    list: [
+      {
+        pagePath: "pages/index/index",
+        text: "首页",
+        iconPath: "assets/icon/index.png",
+        selectedIconPath: "assets/icon/index.png"
+      },
+      {
+        pagePath: "pages/my/my",
+        text: "我的",
+        iconPath: "assets/icon/my.png",
+        selectedIconPath: "assets/icon/my.png"
+      },
+    ],
+  },
 })
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,51 +1,147 @@
-import { View, ScrollView } from '@tarojs/components'
-import Taro, { useReachBottom } from '@tarojs/taro'
+import { View, Text, ScrollView } from '@tarojs/components'
+import Taro, { usePageScroll, useReachBottom } from '@tarojs/taro'
 import { useLoad } from '@tarojs/taro'
-import { AtSearchBar } from 'taro-ui'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.scss'
-import MyTabBar from '../../components/MyTabBar'
 import IndexListItem from '../../components/IndexListItem'
-
+import IndexSearchBar from '../../components/IndexSearchBar'
 
 export default function Index() {
+  const data = [
+    {
+      id: 1,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 2,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 3,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 4,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 5,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 6,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 7,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 8,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 9,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+  ]
+  //页面数据
   const [listData, setListData] = useState([])
-  useLoad(() => {
-    console.log('Page loaded.')
-    const data = [] //获取接口数据
+  //监听页面滚动
+  const [pageScrollValue, setPageScrollValue] = useState(0)
+  //判定是否显示回顶按钮
+  const [isGoingToUp, setIsGoingToUp] = useState(false)
+  //判定是否到达底部
+  const [isEnd, setIsEnd] = useState(false)
+  //页面加载判定
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    //获取接口数据
 
     setListData(data)
+    setIsLoading(false)
+  }, [])
+
+  useLoad(() => {
+    console.log('Page loaded.')
   })
 
-  const [queryValue, setQueryValue] = useState('')
+  useReachBottom(() => {
+    console.log('onReachBottom')
+    setIsEnd(true)
+  })
 
+  //监测用户页面滑动
+  usePageScroll((res) => {
+    // console.log(res.scrollTop)
+    if (res.scrollTop > 200) {
+      setIsGoingToUp(true)
+    }
+    else {
+      setIsGoingToUp(false)
+    }
+  })
 
+  function goToTop() {
+    Taro.pageScrollTo({ scrollTop: 0 })
+  }
 
   return (
     <>
-      <AtSearchBar
-        actionName='搜一下'
-        value={queryValue}
-        onChange={value => setQueryValue(value)}
-        onActionClick={() => {
-          // 处理搜索逻辑
-          console.log('执行搜索操作');
-        }}
-      />
-      <ScrollView className='indexScrollViewArea' scrollX scrollWithAnimation='true'>
+      <IndexSearchBar />
+      {
+        isGoingToUp ? <View className="toUpButton" onClick={goToTop}>
+          <Text className='toUpBtnInner'>↑</Text>
+        </View> : <></>
+      }
+      <ScrollView className='indexScrollViewArea' scrollX scrollWithAnimation scrollTop={0}>
         {
-          listData.map((item, index) => {
-            <IndexListItem imgsrc={item.imgsrc} title={item.title} avatar={item.avatar} username={item.username} readnum={item.readnum} id={item.id} />
-          })
+          isLoading ? <View className='indexLoading'>加载中...</View> :
+            listData.map((item, index) => (
+              <IndexListItem props={{ imgsrc: item.imgsrc, title: item.title, avatar: item.avatar, username: item.username, readnum: item.readnum, id: item.id }} />
+            ))
         }
-        <IndexListItem />
-        <IndexListItem />
-        <IndexListItem />
-        <IndexListItem />
-        <IndexListItem />
-        <IndexListItem />
       </ScrollView>
-      <MyTabBar />
+      {
+        isEnd ? <View className='indexEndText'>已经到底部了....</View> : <></>
+      }
     </>
   )
 }
