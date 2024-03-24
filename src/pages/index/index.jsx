4d62a02840cac61971ab64c2f246bb7ddb14242e
finabin <13803371+finabin@user.noreply.gitee.com>
2024-03-24 22:22:17 +0800

    首页瀑布流加载的实现Demo

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -7,7 +7,7 @@ import IndexListItem from '../../components/IndexListItem'
 import IndexSearchBar from '../../components/IndexSearchBar'
 
 export default function Index() {
-  const data = [
+  let data1 = [
     {
       id: 1,
       title: '丽江三天两晚双人游',
@@ -56,6 +56,9 @@ export default function Index() {
       readnum: 666,
       imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
     },
+  ]
+
+  let data2 = [
     {
       id: 7,
       title: '丽江三天两晚双人游',
@@ -80,7 +83,57 @@ export default function Index() {
       readnum: 666,
       imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
     },
+    {
+      id: 10,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 11,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 12,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 13,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 14,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
+    {
+      id: 15,
+      title: '丽江三天两晚双人游',
+      avatar: 'https://jdc.jd.com/img/200',
+      username: 'admin',
+      readnum: 666,
+      imgsrc: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fss2.meipian.me%2Fusers%2F1184429%2F6afcb2f884248d505f0f40c36f70401e.jpg%3Fmeipian-raw%2Fbucket%2Fivwen%2Fkey%2FdXNlcnMvMTE4NDQyOS82YWZjYjJmODg0MjQ4ZDUwNWYwZjQwYzM2ZjcwNDAxZS5qcGc%3D%2Fsign%2Ff37add58bebac2df4763c3d58c435bd9.jpg&refer=http%3A%2F%2Fss2.meipian.me&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1713786862&t=7a04b13ed59ecf353068a96f8a0a7739'
+    },
   ]
+
+
   //页面数据
   const [listData, setListData] = useState([])
   //监听页面滚动
@@ -95,7 +148,7 @@ export default function Index() {
   useEffect(() => {
     //获取接口数据
 
-    setListData(data)
+    setListData(data1)
     setIsLoading(false)
   }, [])
 
@@ -105,19 +158,48 @@ export default function Index() {
 
   useReachBottom(() => {
     console.log('onReachBottom')
-    setIsEnd(true)
+
   })
 
   //监测用户页面滑动
   usePageScroll((res) => {
-    // console.log(res.scrollTop)
+    console.log(res.scrollTop)
     if (res.scrollTop > 200) {
       setIsGoingToUp(true)
     }
     else {
       setIsGoingToUp(false)
     }
+    setPageScrollValue(res.scrollTop)
   })
+
+  const handleScrollToLower = () => {
+    console.log(125555);
+    // 滚动到距离底部 50px 时触发加载更多数据
+    if (data2.length >= 4) {
+      setIsLoading(true)
+      const newData = []
+      for (let i = 0; i < 4; i++) {
+        const temp = data2.shift()
+        newData.push(temp)
+      }
+      setListData([...listData, ...newData])
+      setIsLoading(false)
+    }
+    else if (data2.length < 4 && data2.length > 0) {
+      setIsLoading(true)
+      const newData = []
+      for (let i = 0; i < data2.length; i++) {
+        const temp = data2.shift()
+        newData.push(temp)
+      }
+      setListData([...listData, ...newData])
+      setIsLoading(false)
+    }
+    else {
+      setIsEnd(true)
+    }
+  }
 
   function goToTop() {
     Taro.pageScrollTo({ scrollTop: 0 })
@@ -131,7 +213,7 @@ export default function Index() {
           <Text className='toUpBtnInner'>↑</Text>
         </View> : <></>
       }
-      <ScrollView className='indexScrollViewArea' scrollX scrollWithAnimation scrollTop={0}>
+      <ScrollView className='indexScrollViewArea' scrollY scrollWithAnimation scrollTop={pageScrollValue} lowerThreshold={50} onScrollToLower={handleScrollToLower}>
         {
           isLoading ? <View className='indexLoading'>加载中...</View> :
             listData.map((item, index) => (
