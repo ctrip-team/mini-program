561ca4b335795c3f9cbb7cd732b72f78a624ea1e
yz <274804397@qq.com>
2024-04-06 19:43:06 +0800

    上传图片支持图片压缩

--- src/components/ImagePicker/index.jsx
+++ src/components/ImagePicker/index.jsx
@@ -1,4 +1,4 @@
-import { View, Image, Text, ScrollView } from '@tarojs/components'
+import { View, Image, Text, ScrollView, Canvas } from '@tarojs/components'
 import { AtIcon } from 'taro-ui'
 import Taro from '@tarojs/taro'
 import './index.scss'
@@ -9,19 +9,23 @@ export default function index({ imageCount, setImageCount, imageFiles, setImageF
             count: 9, // 一次最多可选照片数量
             sizeType: ['original', 'compressed'],
             sourceType: ['album', 'camera'],
-            success: function (res) {
+            success: async function (res) {
                 console.log('res', res);
                 var tempFilePaths = res.tempFilePaths
 
                 // 压缩图片
-                tempFilePaths.forEach((item) => {
-                    Taro.compressImage({
-                        src: item, // 图片路径
-                        quality: 10 // 压缩质量
-                    })
-                })
+                const compressImages = []
+                for (const item of tempFilePaths) {
+                    let imgInfo = await getImgInfo(item);
+                    console.log('imgInfo', imgInfo);
+                    let ctxInfo = await compressImage(imgInfo);
+                    console.log('ctxInfo', ctxInfo);
+                    compressImages.push(ctxInfo.tempFilePath);
+                }
+
                 setImageCount(imageCount + res.tempFilePaths.length)
-                setImageFiles([...imageFiles, ...tempFilePaths])
+                // setImageFiles([...imageFiles, ...tempFilePaths])
+                setImageFiles([...imageFiles, ...compressImages])
                 sethaveImage(true)
             }
         })
@@ -43,8 +47,101 @@ export default function index({ imageCount, setImageCount, imageFiles, setImageF
             urls: imageFiles
         })
     }
+
+    const compressImage = async (file, config = { maxWidth: 1024, maxHeight: 768 }) => {
+        try {
+            let ctxInfo = await new Promise((resolve, reject) => {
+                // 获取图片原始宽高
+                let width = file.width
+                let height = file.height
+
+                // 计算图片当前大小和目标大小的比例：目标大小 / 图片当前大小
+                // 根据比例调整图片的尺寸：
+                // 新宽度 = 原始宽度 * √(目标大小 / 图片当前大小) 
+                // 新高度 = 原始高度 * √(目标大小 / 图片当前大小)
+                // 宽高同比例调整
+                // 宽度 > 最大限宽 -> 重置尺寸
+                if (width > config.maxWidth) {
+                    const ratio = config.maxWidth / width
+                    width = config.maxWidth
+                    height = height * ratio
+                }
+                // 高度 > 最大限高度 -> 重置尺寸
+                if (height > config.maxHeight) {
+                    const ratio = config.maxHeight / height
+                    width = width * ratio
+                    height = config.maxHeight
+                }
+
+                // 获取canvas元素
+                const query = Taro.createSelectorQuery()
+                let dom = query.select(`#compressCanvas`)
+                dom.fields({ node: true, size: true })
+                    .exec((res) => {
+                        // Canvas 对象
+                        const canvas = res[0].node
+                        // 渲染上下文
+                        const ctx = canvas.getContext('2d')
+
+                        // 根据设备像素比处理尺寸 = 大小 * 设备像素
+                        const dpr = Taro.getSystemInfoSync().pixelRatio
+                        canvas.width = width * dpr
+                        canvas.height = height * dpr
+                        ctx.scale(dpr, dpr)
+
+                        //创建img对象
+                        let img = canvas.createImage();
+                        img.src = file.path; // 给图片添加路径
+                        //图片加载完毕
+                        img.onload = function () {
+                            // 将图片绘制到 canvas
+                            ctx.drawImage(img, 0, 0, width, height)
+                            // 生成图片
+                            Taro.canvasToTempFilePath({
+                                canvas,
+                                x: 0,
+                                y: 0,
+                                destWidth: width,
+                                destHeight: height,
+                                success(res) {
+                                    resolve(res); // 生成临时文件路径
+                                }
+                            })
+                        }
+                    })
+            })
+            return ctxInfo
+        } catch (err) { console.log(err); }
+    }
+
+    const getImgInfo = async (tempFilePath) => {
+        try {
+            let image = await new Promise((resolve, reject) => {
+                Taro.getImageInfo({
+                    src: tempFilePath,
+                    success(res) {
+                        let imgInfo = {
+                            type: res.type,
+                            height: res.height,
+                            width: res.width,
+                            path: res.path
+                        }
+                        resolve(imgInfo)
+                    },
+                    fail(err) {
+                        reject(err)
+                    }
+                })
+            })
+            return image
+        } catch (err) { console.log(err); }
+    }
+
     return (
         <>
+            <View>
+                <Canvas id="compressCanvas" class="canvas-compress" type="2d" />
+            </View>
             <View className='add-image-container'>
                 {imageFiles.map((item, index) => {
                     return (
--- src/pages/PublishPage/index.jsx
+++ src/pages/PublishPage/index.jsx
@@ -146,7 +146,6 @@ export default function Index() {
     }
   }
 
-
   return (
     <>
       <View className='publish-container'>
