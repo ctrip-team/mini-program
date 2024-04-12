8762a0a27f883f22584d145e56713437af52c69f
yz <274804397@qq.com>
2024-04-12 10:35:59 +0800

    整理修改注释

--- src/components/ImagePicker/index.jsx
+++ src/components/ImagePicker/index.jsx
@@ -48,36 +48,25 @@ export default function index({ imageCount, setImageCount, imageFiles, setImageF
     const compressImage = async (file, config = { maxWidth: 1024, maxHeight: 768 }) => {
         try {
             let ctxInfo = await new Promise((resolve, reject) => {
-                // 获取图片原始宽高
                 let width = file.width
                 let height = file.height
 
-                // 计算图片当前大小和目标大小的比例：目标大小 / 图片当前大小
-                // 根据比例调整图片的尺寸：
-                // 新宽度 = 原始宽度 * √(目标大小 / 图片当前大小) 
-                // 新高度 = 原始高度 * √(目标大小 / 图片当前大小)
-                // 宽高同比例调整
-                // 宽度 > 最大限宽 -> 重置尺寸
                 if (width > config.maxWidth) {
                     const ratio = config.maxWidth / width
                     width = config.maxWidth
                     height = height * ratio
                 }
-                // 高度 > 最大限高度 -> 重置尺寸
                 if (height > config.maxHeight) {
                     const ratio = config.maxHeight / height
                     width = width * ratio
                     height = config.maxHeight
                 }
 
-                // 获取canvas元素
                 const query = Taro.createSelectorQuery()
                 let dom = query.select(`#compressCanvas`)
                 dom.fields({ node: true, size: true })
                     .exec((res) => {
-                        // Canvas 对象
                         const canvas = res[0].node
-                        // 渲染上下文
                         const ctx = canvas.getContext('2d')
 
                         // 根据设备像素比处理尺寸 = 大小 * 设备像素
@@ -86,7 +75,6 @@ export default function index({ imageCount, setImageCount, imageFiles, setImageF
                         canvas.height = height * dpr
                         ctx.scale(dpr, dpr)
 
-                        //创建img对象
                         let img = canvas.createImage();
                         img.src = file.path; // 给图片添加路径
                         //图片加载完毕
